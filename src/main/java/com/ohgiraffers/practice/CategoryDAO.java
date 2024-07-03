5cc7a5e36266f38ec6153f8baf24c86297e7878a
apfp77 <apfp77@naver.com>
2024-07-03 15:18:52 +0900

    feat: category delete

--- src/main/java/com/ohgiraffers/practice/CategoryDAO.java
+++ src/main/java/com/ohgiraffers/practice/CategoryDAO.java
@@ -1,4 +1,27 @@
 package com.ohgiraffers.practice;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static com.ohgiraffers.common.Template.close;
+
 public class CategoryDAO {
+    public int deleteMenu(Connection con, Long id) {
+        PreparedStatement ps = null;
+        int result = 0;
+        String query = "DELETE FROM category WHERE id = ?";
+
+        try {
+            ps = con.prepareStatement(query);
+            ps.setLong(1, id);
+            result = ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(ps);
+        }
+        return result;
+    }
 }
