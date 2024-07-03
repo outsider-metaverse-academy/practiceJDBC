781db818a5cecbeb0e96626230b6e6a99550f336
jeonjinhyun <jjh06500@gmail.com>
2024-07-03 16:24:58 +0900

    select

---
+++ src/main/java/com/ohgiraffers/select/ApplicationSelect.java
@@ -0,0 +1,21 @@
+package com.ohgiraffers.select;
+
+import com.ohgiraffers.model.dto.CategoryDTO;
+
+import java.util.Scanner;
+
+public class ApplicationSelect {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("조회하실 메뉴의 코드 :");
+        int categpryCpde = sc.nextInt();
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setCategoryCode(categpryCpde);
+        CategorySelectService categorySelectService = new CategorySelectService();
+        if((categoryDTO=categorySelectService.selectCategory(categoryDTO))!=null){
+            System.out.println(categoryDTO);
+        }else{
+            System.out.println("실패하셨습니다.");
+        }
+    }
+}
